ba022a8d623866509f675162ab0487fe9ae20eee
Scott Walter <scott.walter@nice.com>
2024-03-14 11:23:59 -0500

    first checking

---
+++ vim-sandbox.js
@@ -0,0 +1,23 @@
+function fred(name) {
+  console.log("Hello there", name);
+  console.log("Hello there");
+  console.log("Hello there");
+  console.log("Hello there");
+  console.log("Hello there"); //hello
+  console.log("Hello there");
+  console.log("Hello there");
+  console.log("hello there");
+  console.log("Hello there");
+  console.log("Hello there");
+  console.log("Hello there");
+
+  if (name === "scott") {
+    console.log("fred walter");
+  }
+
+  if (true) {
+  }
+}
+
+fred();
+barny();
